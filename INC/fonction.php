d5a06c779f27f0b0c5c9796f74e5b9fe0a56684f
NyAiko <nyaikoroimpitiaarimanana@gmail.com>
2025-06-28 19:53:06 +0300

    fix fonction.php

--- INC/fonction.php
+++ INC/fonction.php
@@ -6,7 +6,7 @@
     }
 
     function getEmployees($data,$id_dept){
-        $code="SELECT * FROM dept_manager join departments on dept_manager.dept_no=departments.dept_no join employees on employees.emp_no=dept_manager.emp_no where departments.dept_no='$id_dept'";
+        $code="SELECT * FROM dept_emp join departments on dept_emp.dept_no=departments.dept_no join employees on employees.emp_no=dept_emp.emp_no where departments.dept_no='$id_dept'";
         $resultat = mysqli_query($data, $code);
         return $resultat;
     }
