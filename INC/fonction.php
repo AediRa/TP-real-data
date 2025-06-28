30d3a45e424c45089848049267f1859affb3a0d1
NyAiko <nyaikoroimpitiaarimanana@gmail.com>
2025-06-28 20:15:05 +0300

    fix fonction.php

--- INC/fonction.php
+++ INC/fonction.php
@@ -1,6 +1,6 @@
 <?php
     function getDepartment($data){
-        $code="SELECT * FROM dept_manager join departments on dept_manager.dept_no=departments.dept_no join employees on employees.emp_no=dept_manager.emp_no";
+        $code="SELECT * FROM dept_manager join departments on dept_manager.dept_no=departments.dept_no join employees on employees.emp_no=dept_manager.emp_no where dept_manager.to_date > now()";
         $resultat = mysqli_query($data, $code);
         return $resultat;
     }
