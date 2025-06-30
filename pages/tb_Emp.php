0a8eb2ed57775bdf4c916982d6a0033a1f899703
NyAiko <nyaikoroimpitiaarimanana@gmail.com>
2025-06-30 10:00:13 +0300

    format tb_Department.php

--- pages/tb_Emp.php
+++ pages/tb_Emp.php
@@ -1,4 +1,5 @@
 <?php
+    ob_start();
     session_start();
     include("../INC/fonction.php");
     include("data.php");
@@ -6,17 +7,7 @@
     $id_dept = $_SESSION['id_dept'];
     $result = getEmployees($data,$id_dept);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css-2/style.css">
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <script src="../assets/js/bootstrap.bundle.min.js.map"></script>
-    <title>Document</title>
-</head>
-<body>
+
     <div class="container-fluid">
         <h3><?= $name_dept ?></h3>
         <ul>
@@ -25,5 +16,9 @@
             <?php } ?>
         </ul>
     </div>
-</body>
-</html>
+
+<?php
+    $nav="ok";
+    $contenu = ob_get_clean();
+    include("html.php");
+?>
