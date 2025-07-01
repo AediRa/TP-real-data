e806c6fba91c7fd50382e0aa935d7f7c5d6b3f0e
NyAiko <nyaikoroimpitiaarimanana@gmail.com>
2025-07-01 17:07:40 +0300

    FIXE view

--- sql/view.sql
+++ sql/view.sql
@@ -5,4 +5,12 @@ CREATE OR REPLACE VIEW v_dept_deptEmp_Emp AS
 SELECT birth_date,first_name,last_name ,gender,hire_date,id_dept,dept_name,from_date,to_date,employees.emp_no as id_emp FROM v_dept_deptEmp join employees on employees.emp_no = v_dept_deptEmp.emp_no ;
 
 CREATE OR REPLACE VIEW v_IdDept_NbEmp AS
-SELECT id_dept,count(*) from v_dept_deptEmp_Emp group by id_dept;
+SELECT id_dept,count(*) as nb_empBydept from v_dept_deptEmp_Emp group by id_dept;
+
+CREATE OR REPLACE VIEW v_NbF AS
+SELECT gender,count(*) FROM v_dept_deptEmp_Emp where gender="F";
+
+CREATE OR REPLACE VIEW v_NbM AS
+SELECT gender,count(*) FROM v_dept_deptEmp_Emp where gender="M";
+
+select sum(nb_empBydept) from v_IdDept_NbEmp;
